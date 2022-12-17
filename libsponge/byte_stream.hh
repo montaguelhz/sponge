9d6db196c6403545bf7f3e3a801a07c99422cdda
montague <laohuazheng@126.com>
2022-12-17 23:01:02 +0800

    lab0

--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -1,8 +1,8 @@
 #ifndef SPONGE_LIBSPONGE_BYTE_STREAM_HH
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
+#include <deque>
 #include <string>
-
 //! \brief An in-order byte stream.
 
 //! Bytes are written on the "input" side and read from the "output"
@@ -16,7 +16,11 @@ class ByteStream {
     // all, but if any of your tests are taking longer than a second,
     // that's a sign that you probably want to keep exploring
     // different approaches.
-
+    std::deque<char> _queue;
+    size_t _capacity_size;
+    size_t _read_size{};
+    size_t _written_size{};
+    bool _end_input{};
     bool _error{};  //!< Flag indicating that the stream suffered an error.
 
   public:
